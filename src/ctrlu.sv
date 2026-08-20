18a4109cfec02e660cac0b0842cd5c695aeb6d06
ishaank1607 <ikhambas@purdue.edu>
2026-08-19 21:47:30 -0400

    Refactor control logic in ctrlu.sv

--- src/ctrlu.sv
+++ src/ctrlu.sv
@@ -31,13 +31,12 @@ module ctrlu (
     reg_read_addr = operand[2:0];
     acc_src = 2'b00;
     out_enable = 0;
-    sub_cin = 0;
-      
+    
     case ( opcode )
       
       4'b0001: begin
-        acc_src = 2'b01;
-        acc_load = 1;
+        acc_src = 1;
+        acc_load = 2'b01;
         pc_increment = 1;
       end
       
@@ -59,7 +58,6 @@ module ctrlu (
         acc_load = 2'b01;
         reg_write_enable = 0;
         pc_increment = 1;
-        sub_cin = 1;
       end
       
       4'b0101:begin
@@ -110,15 +108,19 @@ module ctrlu (
       end
       
       4'b1100: begin
-        if (zflag)
+        if (zflag) begin
           pc_load = 1;
+          pc_increment = 0;
+        end
         else
           pc_increment = 1;
       end
       
       4'b1101: begin
-        if (cflag)
+        if (cflag) begin
           pc_load = 1;
+          pc_increment = 0;
+        end
         else
           pc_increment = 1;
       end
